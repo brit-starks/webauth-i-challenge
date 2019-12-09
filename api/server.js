a51ceb8e5dd96d03c36104c113a7fd8be14bc8e0
brit-starks <brittneytechiethomas@gmail.com>
2019-12-09 11:11:08 -0800

    Add server

--- api/server.js
+++ api/server.js
@@ -2,3 +2,11 @@ const express = require('express');
 const helmet = require('helmet');
 
 const server = express();
+
+server.use(helmet());
+
+server.get('/', (req, res) => {
+  res.json({api: "WebAuth-I-Challenge"});
+})
+
+module.exports = server;
