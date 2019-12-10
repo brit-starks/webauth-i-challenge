dd4f0833454b8e0b55c5ef458cda199fb59d988f
brit-starks <brittneytechiethomas@gmail.com>
2019-12-09 19:39:06 -0800

    Add access functions, add router (success)

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const userRouter = require('../users/users-router');
+
 const server = express();
 
 server.use(helmet());
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   res.json({api: "WebAuth-I-Challenge"});
--- users/users-router.js
+++ users/users-router.js
@@ -2,3 +2,12 @@ const router = require('express').Router();
 
 const dbUser = require('./users-model');
 
+router.get('/', (req, res) => {
+  dbUser.find()
+    .then(user => {
+      res.json(user)
+    })
+    .catch(err => res.json(err));
+});
+
+module.exports = router;
