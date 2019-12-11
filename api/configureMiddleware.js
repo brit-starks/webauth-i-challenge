3acd75cc3a63394e884d6260981d85509074756b
brit-starks <brittneytechiethomas@gmail.com>
2019-12-10 20:26:44 -0800

    Add user session (success)

--- api/configureMiddleware.js
+++ api/configureMiddleware.js
@@ -4,16 +4,15 @@ const cors = require('cors');
 const session = require('express-session');
 
 const sessionConfig = {
-  name: 'bats-cookie', //default is named sid, easier for hackers
-  secret:  'Shh, it\'s a secret',  // what we use to encrypt and decrypt the cookie
+  name: 'bats-cookie', 
+  secret:  'Shh, it\'s a secret',
   cookie: {
-    maxAge: 1000 * 30,  // 30s how long you want the session to stay open (milliseconds)
-    secure: false,  // Can you send the cookie over an un-encrypted connection? (HTTP) change to 'True' in production
-    httpOnly: true // always 'true', this means "This cookie cannot be accessed with javaScript"
+    maxAge: 1000 * 30,
+    secure: false,
+    httpOnly: true
   },
-  resave: false, // do we want to recreate a session although nothing has changed, no, avoid chatter to our session storage
-  saveOnInitialized: false //this needs to be dynamically changed. GDPR Compliance, there are laws against setting cookies automatically
-                           // dynamically we must set options for user to accept if they want to  
+  resave: false,
+  saveOnInitialized: false
 };
 
 module.exports = server => {
--- auth/auth-required-middleware.js
+++ auth/auth-required-middleware.js
@@ -1,18 +1,7 @@
-const users = require('../users/users-model');
-const bcrypt = require('bcryptjs');
-
 module.exports = (req,res,next) => {
-  const { username, password } = req.headers
-
-  users.findBy({ username })
-  .first()
-  .then( _user=>{
-  if( _user && bcrypt.compareSync(password, _user.password ))
-  {
+  if (req.session && req.session.user){
   next()
   } else {
       res.status(401).json({message: 'Invalid Credentials'})
   }
-  })
-  .catch( err => res.status(500).json({message: err}) )
-}
+};
