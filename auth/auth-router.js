30be39dd1464f6f2d857e7c46d7e0ab938bd3f21
brit-starks <brittneytechiethomas@gmail.com>
2019-12-10 18:17:52 -0800

    Add login autherization using bcrypt

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -23,4 +23,29 @@ router.post('/register', (req, res) => {
     });
 });
 
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  User.findBy({ username })
+  .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({
+          message: `Welcome ${user.username}!`
+        });
+      } else {
+        res.status(401).json({
+          message: 'Invalid Credentials'
+        });
+      }
+    })
+    .catch( err => {
+      res.status(500).json({
+        err, 
+        message: 'Opps, something happened on our end...'
+      });
+    });
+})
+
 module.exports = router;
